0310e2118ba630af7abf64728775c6b55061dd93
kaiserawu <kaiser.a.wu@gmail.com>
2019-05-11 11:33:50 -0700

    Create POST endpoint

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -17,7 +17,7 @@ router.get('/:id', async (req, res) => {
   try {
     const postId = req.params.id;
     const post = await db.findById(postId);
-    
+
     if (post) {
       res.status(200).json(post);
     } else {
@@ -25,6 +25,20 @@ router.get('/:id', async (req, res) => {
     }
   } catch(err) {
     res.status(500).json({ error: "The post information could not be retrieved." });
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const postData = req.body;
+    if (postData.title && postData.contents) {
+      const saved = await db.insert(postData);
+      res.status(201).json(saved);
+    } else {
+      res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    }
+  } catch {
+    res.status(500).json({ error: "There was an error while saving the post to the database" });
   }
 })
 
