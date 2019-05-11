1ecf5f97df8ab9d6705fe01b09043bfcb4203157
kaiserawu <kaiser.a.wu@gmail.com>
2019-05-11 09:41:17 -0700

    Set up posts router

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+const postRoutes = require('./posts/postRoutes');
+
 const server = express();
 
 server.use(express.json());
-
+server.use('/api/posts', postRoutes);
 
 server.listen(4000, () => {
   console.log('Server running on port 4000');
